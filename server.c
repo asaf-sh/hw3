15978a7de4372c897ecdd030eeec9e736343f3e3
asaf-sh <asaf.shoham8@gmail.com>
2022-06-15 12:15:32 +0300

    main workflow

--- server.c
+++ server.c
@@ -9,18 +9,9 @@
 #define DH ("dh")
 #define RANDOM ("random")
 
-enum schedalg {block, dt, dh, random};
-// 
-// server.c: A very, very simple web server
-//
-// To run:
-//  ./server <portnum (above 2000)>
-//
-// Repeatedly handles HTTP requests sent to this port number.
-// Most of the work is done within routines written in request.c
-//
+enum schedalg {block, drop_tail, drop_head, drop_random};
+enum req_type {static, dynamic};
 
-// HW3: Parse the new arguments too
 typedef struct thread_stats_t{
     int tid;
     int req_count;
@@ -30,6 +21,7 @@ typedef struct thread_stats_t{
 
 typedef struct req_t{
     int fd;
+    enum req_type type;
     struct timeval arrival_time;
     struct timeval dispatch_interval;
     TStats handler_thread_stats;
@@ -37,15 +29,28 @@ typedef struct req_t{
 
 typedef struct queue_t{
     int max;
-    int size;
-    void **arr;
-    int head,tail;
+    int drop;
+    int size;  //pending count
+    int total_count;
+    Req *pendings;
+    int tail;
 } *Queue;
+
+void req_destroy(Req req){
+    close(req->fd);
+    if(req->handler_thread_stats)
+        free(req->handler_thread_stats);
+    free(req);
+}
 
 bool q_initialize(Queue q, int max_size);
 void q_destroy(Queue q);
 void q_push(Queue q, void* val);
-void* q_pop(Queue q);
+Req q_pop(Queue q);
+Req q_get(Queue q, int pos);
+void q_set(Queue q, int pos, Req req);
+
+Req create_new_request(int connfd);  // TADA!
 
 
 void getargs(int argc, char *argv[], int *port, int* thread_count, int *queue_size, enum schedalg *alg)
@@ -76,7 +81,9 @@ void getargs(int argc, char *argv[], int *port, int* thread_count, int *queue_si
     }
 }
 
+//TADA
 void* work(void* ptr) {
+    Queue q = (Queue) ptr;
     while (requests == 0) {
         pthread_cond_wait(&pend_and_free, &global_lock);
         pthread_mutex_lock(&global_lock);
@@ -88,116 +95,150 @@ void* work(void* ptr) {
 
 }
 
-bool addRequest(int fd, int pendings_size, int** pendings) {
-    int i = 0;
-    while (&pendings[i] == -1 && i < pendings_size) {
-        i++;
-    }
-    if (i < pendings_size) {
-        &pendings[i] = fd;
-        return true;
-    }
-    else {
-        return false;
+static void initialize_threads(int thread_count, pthread_t *workers, Queue q){
+    for (i = 0, i < thread_count, i++) {
+        pthread_create(&workers[i], NULL, work, q);
     }
 }
 
-static void initialize_threads(int thread_count, pthread_t *workers){
-    for (i = 0, i < thread_count, i++) {
-        pthread_create(&workers[i], NULL, work, NULL);
-    }
-}
-static void initialize_pendings(int max_requests, int *pendings){
-    for (i = 0, i < max_requests, i++) {
-        pendings[i] = EMPTY_REQ;
-    }
+
+//with lock
+static inline is_overload(Queue q){
+    return q->total_count == q->max;
 }
 
 int main(int argc, char *argv[])
 {
-    int listenfd, connfd, port, clientlen;
+    int listenfd, connfd, port, clientlen, queue_size, thread_count;
+    enum sched alg;
     struct sockaddr_in clientaddr;
 
-    getargs(&port, argc, argv);
+    getargs(argc, argv, &port, &thread_count, &queue_size, &alg);
 
-    int pendings_size = argv[3];
-    int thread_count = args[2];
 
     int busy_threads = 0;
     int requests = 0;
 
-    pthread_mutex_t global_lock;
-    pthread_cond_t pend_and_free;
+    //in use for push&pop pending request by main | workers
+    pthread_mutex_t global_lock;  //for every access to req_queue and for conditions
+    pthread_cond_t pend_and_free;  //availiable pending for workers
+    pthread_cond_t queue_not_full;  //availiable slots for new pendings (for 'block' schedlag)
 
+    struct queue_t req_queue;
+    q_initialize(&req_queue, queue_size);
 
-    int *pendings = malloc(pendings_size * sizeof(int));
     pthread_t *workers = malloc(thread_count * sizeof(pthread_t));
-
-    initialize_pendings(pendings_size, pendings);
     initialize_threads(thread_count, workers);
-    
-
-    // 
-    // HW3: Create some threads...
-    //
 
     listenfd = Open_listenfd(port);
     while (1) {
-	clientlen = sizeof(clientaddr);
-	connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
-    pthread_mutex_lock(&global_lock);
-    accept_to_pend = addRequest(connfd, *pendings);
-    if (!accept_to_pend) {
-        handleOverload();
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
+        Req new_req = create_new_req(connfd);  //consider moving to inside the global lock
+        pthread_mutex_lock(&global_lock);
+        if (is_overload(&req_queue)){
+            switch (alg)
+            {
+            case block:
+                while(is_overload(&req_queue)){
+                    pthread_cond_wait(&queue_not_full, &global_lock);
+                    pthread_mutex_lock(&global_lock)
+                }
+                break;
+            case drop_tail:
+                break;
+            case drop_random:
+                q_drop_random(&req_queue);
+                break;
+            default: //drop_head
+                q_del(&req_queue, 0); 
+                break;
+            }
+        }
+        
+        if(is_overload(&req_queue)) //for (drop_tail) and (drop_random with no pendings) cases
+            req_destroy(new_req);
+
+        else{
+            q_push(&req_queue, new_req);
+            pthread_cond_signal(&pend_and_free);
+        }
+        pthread_mutex_unlock(&global_lock);
     }
-    pthread_cond_signal(&pend_and_free);
-
-
-	// 
-	// HW3: In general, don't handle the request in the main thread.
-	// Save the relevant info in a buffer and have one of the worker threads 
-	// do the work. 
-	// 
-	requestHandle(connfd);
-
-	Close(connfd);
-    }
-
 }
 
+//with lock
 bool q_initialize(Queue q, int max_size){
-    if(max_size <= 0 || (q->arr = malloc(size(void*) * max_size))==NULL)
+    if(max_size <= 0 || (q->pendings = malloc(size(Req) * max_size))==NULL)
         return false;
+    q->max = max_size;
+    q->drop = ceil((double)max_size * 0.3)
     q->size = 0;
-    q->head = 0;
+    q->total_count = 0;
     q->tail = 0;
     return true;
 }
+
+//with lock
 //naive destroy (meanng only frees its value, rather then calling generic given destroy function)
 void q_destroy(Queue q){
-    for(int i=q->tail; q->size > 0; i=(i+1)%(q->max)){
-        free(q->arr[i]);
-        q->size--;
+    for(int pos=0; pos < q->size; ++pos){
+        free(q_get(q, pos));
     }
-    free(q->arr);
+    free(q->pendings);
 }
 
-void q_push(Queue q, void* val){
-    q->arr[q->head] = val;
-    q->head = (q->head)%(q->max);
+//with lock
+void q_push(Queue q, Req req){
+    if(q->total_count == q->max)
+        return; // consider printing err or returning false
+    q_set(q,q->size, req);
     q->size++;
+    q->total_count++;
 }
 
-void* q_pop(Queue q){
+//with lock
+Req q_pop(Queue q){
     if (q->size == 0)
         return NULL;
-    void* val = q->arr[q->tail];
+    Req req = q_get(q, 0);
     q->tail = (q->tail+1)%(q->max);
     q->size--;
-    return val;
+    // we preform the q->total_count-- only after worker finished handling request
+    return req;
 }
 
-    
+//with lock
+Req q_get(Queue q, int pos){
+    return q->pendings[(q->tail+pos)%q->max];
+}
+
+//with lock
+void q_set(Queue q, int pos, Req req){
+    q->pendings[(q->tail+pos)%q->max] = req;
+}
+
+//with lock
+bool q_del(Queue q, int pos){
+    if(pos < 0 || pos>q->size)
+        return false;
+    req_destroy(q_get(q,pos));
+    q->size--;
+    q->total_count--;
+    return true;
+}
+
+//with lock
+void q_drop_random(Queue q){
+    for(int i=0; 0<(q->size) && i<(q->drop); ++i){
+        int pos = rand() % q->size;
+        q_del(q,pos);
+        //updating queue (narrowing gaps)
+        for(int j=0; j<pos; ++j){
+            q_set(q,pos-j, q_get(q, pos-j-1));
+        }
+    }
+}
 
 
  
